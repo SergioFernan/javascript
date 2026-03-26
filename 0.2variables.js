74b5ff81ad901a2a66ae33468dd86589f053393c
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 20:39:12 -0500

    de farenheid a graods y visceversa

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,7 +1,27 @@
-let edadPerro = Number(prompt("iingrese la edad de su perro"));
-let edadHumana = edadPerro * 7;
-console.log("la edad del perro es" , edadHumana);
-
+let tipo = prompt("en que vas a medir, en f o en c")
+if(tipo == "f"){
+    let farenheid = Number(prompt("ingrese los farenheid"));
+    if(farenheid <= 0){
+        console.log("no es numero valido para medir")
+    }
+    else{
+    let grados = (farenheid - 32) / 1.88
+    console.log("su medida en grados es" , grados)
+    }
+}
+else if(tipo == "c"){
+     let grados = parseFloat(prompt("ingrese el valor en grados"));
+     if(grados <= 0){
+        console.log("no es numero valido para medir")
+     }
+     else{
+     let farenheid = (grados * 1.8) +32
+     console.log("su medida en farenheid es" , farenheid)
+     }
+}
+else{
+     console.log("el caracter no es correcto")
+}
 
 
 
