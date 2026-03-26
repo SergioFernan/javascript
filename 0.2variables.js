e8ea7429861fab19a1e9ba05172187478cc367c1
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 20:58:08 -0500

    area de un triangulo

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,27 +1,7 @@
-let tipo = prompt("en que vas a medir, en seg o en min")
-if(tipo == "min"){
-    let minutos = Number(prompt("ingrese los minutos"));
-    if(minutos <= 0){
-        console.log("no es numero valido para medir")
-    }
-    else{
-    let seg = minutos * 60
-    console.log("su medida en segundos es es" , seg)
-    }
-}
-else if(tipo == "seg"){
-     let seg = Number(prompt("ingrese el valor en seg"));
-     if(seg <= 0){
-        console.log("no es numero valido para medir")
-     }
-     else{
-     let minutos = seg / 60
-     console.log("su medida en minutos es" , minutos)
-     }
-}
-else{
-     console.log("el caracter no es correcto")
-}
+let base = Number(prompt("ingrese la base"))
+let altura = Number(prompt("ingrese la altura"))
+let area = (base * altur) /2;
+console.log("el area de un triangulo es" , area)
 
 
 
