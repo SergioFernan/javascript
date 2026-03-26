96933ecb964825ca7847503bee203d08c262fb01
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 20:44:10 -0500

    area de un cuadrado

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,28 +1,6 @@
-let tipo = prompt("en que vas a medir, en f o en c")
-if(tipo == "f"){
-    let farenheid = Number(prompt("ingrese los farenheid"));
-    if(farenheid <= 0){
-        console.log("no es numero valido para medir")
-    }
-    else{
-    let grados = (farenheid - 32) / 1.88
-    console.log("su medida en grados es" , grados)
-    }
-}
-else if(tipo == "c"){
-     let grados = parseFloat(prompt("ingrese el valor en grados"));
-     if(grados <= 0){
-        console.log("no es numero valido para medir")
-     }
-     else{
-     let farenheid = (grados * 1.8) +32
-     console.log("su medida en farenheid es" , farenheid)
-     }
-}
-else{
-     console.log("el caracter no es correcto")
-}
-
+let lado = Number(prompt("ingrese la medida del lado del cuadrado"));
+let area = lado * lado;
+console.log("el area del cuadrado es" ,  area)
 
 
 
