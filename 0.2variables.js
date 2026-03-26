32ee74dc26a76b1e43e94d6f251f99f2280b918a
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 21:03:23 -0500

    de ml a cm y visceversa

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,7 +1,25 @@
-let base = Number(prompt("ingrese la base"))
-let altura = Number(prompt("ingrese la altura"))
-let area = (base * altur) /2;
-console.log("el area de un triangulo es" , area)
-
-
+let tipo = prompt("en que vas a medir, en ml o en cm")
+if(tipo == "ml"){
+    let ml = Number(prompt("ingrese los milimetros"));
+    if(ml <= 0){
+        console.log("no es numero valido para medir")
+    }
+    else{
+    let cm = ml / 10
+    console.log("su medida en cm es" , cm)
+    }
+}
+else if(tipo == "cm"){
+     let cm = parseFloat(prompt("ingrese el valor en cm"));
+     if(cm <= 0){
+        console.log("no es numero valido para medir")
+     }
+     else{
+     let ml = cm * 10
+     console.log("su medida en milimetros es" , ml)
+     }
+}
+else{
+     console.log("el caracter no es correcto")
+}
 
