788be23db322923f8e330a4429e51e27c76e8c0e
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 20:47:03 -0500

    perimetro

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,6 +1,6 @@
 let lado = Number(prompt("ingrese la medida del lado del cuadrado"));
-let area = lado * lado;
-console.log("el area del cuadrado es" ,  area)
+let perimetro = lado * 4;
+console.log("el perimetro del cuadrado es" ,  perimetro)
 
 
 
