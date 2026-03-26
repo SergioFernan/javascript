21668bfb3a7a4a2bcfecf3a0996aaf61fe7f6a8f
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-25 20:18:19 -0500

    edad perro

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,27 +1,6 @@
-let tipo = prompt("en que vas a medir, en km o en m")
-if(tipo == "m"){
-    let metros = Number(prompt("ingrese los metros"));
-    if(metros <= 0){
-        console.log("no es numero valido para medir")
-    }
-    else{
-    let km = metros / 1000
-    console.log("su medida en km es" , km)
-    }
-}
-else if(tipo == "km"){
-     let km = parseFloat(prompt("ingrese el valor en km"));
-     if(km <= 0){
-        console.log("no es numero valido para medir")
-     }
-     else{
-     let metros = km * 1000
-     console.log("su medida en m es" , metros)
-     }
-}
-else{
-     console.log("el caracter no es correcto")
-}
+let edadPerro = Number(prompt("iingrese la edad de su perro"));
+let edadHumana = edadPerro * 7;
+console.log("la edad del perro es" , edadHumana);
 
 
 
