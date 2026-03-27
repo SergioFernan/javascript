683ac21400a092b655cf902751e67dc733b2a107
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-03-26 21:49:18 -0500

    ejercicio interes

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,17 +1,13 @@
-let precio = Number(prompt(`cuanto fue el precio total`));
-let numeroDePersonas = Number(prompt(`cuantas personas comieron`));
-let PersonaQueMasComio = prompt(`la persona que mas comio es`)
-let PersonaQueMenosComio = prompt(`la persona que menos comio es`)
-normal = precio / (numeroDePersonas + 1)
-nombreDeMuertoDeHambre = (precio / (numeroDePersonas + 1)) / 2
-
-let partes = numeroDePersonas + 1;
-let pagoNormal = precio / partes;
-let pagoDoble = pagoNormal * 2;
-let pagoMenos = pagoNormal * 0.5;
+// mojto cuantos meses despues de 6 meses gana tanto dinero del 10% efectivo anual //
 
 
-
-console.log(` pago de cada persona ${pagoNormal.toFixed(2)}`);
-console.log(`${PersonaQueMasComio} que comio mas paga ${pagoDoble.toFixed(2)}`);
-console.log(`${PersonaQueMenosComio} que comio menos paga ${pagoMenos.toFixed(2)}`);
+let monto = Number(prompt("ingrese el valor a depositar en el cdt"));
+let montoA6Meses = monto * (4.98 / 100)
+let montoAMes = monto * (0.83 / 100)
+let montoAAno = monto * (10 / 100)
+let total6 = montoA6Meses + monto
+let total1 = montoAMes + monto
+let total12 = montoAAno + monto
+console.log(`su valor ahorrado en 6 meses es ${montoA6Meses} en total tendrias ${total6}`)
+console.log(`su valor ahorrado en 1 mes es ${montoAMes} en total tendrias ${total1}`)
+console.log(`su valor ahorrado en 12 meses es ${montoAAno} en total tendrias ${total12}`)
