99620235968cb207d42b86307aa4d1944d72f42f
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-04-07 18:52:21 -0500

    ejercicios cumas if else

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,13 +1,30 @@
-// mojto cuantos meses despues de 6 meses gana tanto dinero del 10% efectivo anual //
+let operacion = Number(prompt(`ingrese la operacion que desea realizar
+    1. suma
+    2. resta
+    3. multiplicacion
+    4. division`))
 
+if (operacion <= 0 || operacion > 4 ) {
+    console.log(`operacion no valida`)
+} 
+else if (operacion === (1 || "suma")) {
+    let numero1 = Number(prompt(`ingrese el primer numero`))
+    let numero2 = Number(prompt(`ingrese el segundo numero`))
+    console.log(`el resultado es ${numero1 + numero2}`)
+}
+else if (operacion === (2 || "resta")) {
+    let numero1 = Number(prompt(`ingrese el primer numero`))
+    let numero2 = Number(prompt(`ingrese el segundo numero`))
+    console.log(`el resultado es ${numero1 - numero2}`)
+}
+else if (operacion === (3 || "multiplicacion")){
+    let numero1 = Number(prompt(`ingrese el primer numero`))
+    let numero2 = Number(prompt(`ingrese el segundo numero`))
+    console.log(`el resultado es ${numero1 * numero2}`)
+}
+else if (operacion === (4 || "division")) {
+    let numero1 = Number(prompt(`ingrese el primer numero`))
+    let numero2 = Number(prompt(`ingrese el segundo numero`))
+    console.log(`el resultado es ${numero1 / numero2}`)
+}
 
-let monto = Number(prompt("ingrese el valor a depositar en el cdt"));
-let montoA6Meses = monto * (4.98 / 100)
-let montoAMes = monto * (0.83 / 100)
-let montoAAno = monto * (10 / 100)
-let total6 = montoA6Meses + monto
-let total1 = montoAMes + monto
-let total12 = montoAAno + monto
-console.log(`su valor ahorrado en 6 meses es ${montoA6Meses} en total tendrias ${total6}`)
-console.log(`su valor ahorrado en 1 mes es ${montoAMes} en total tendrias ${total1}`)
-console.log(`su valor ahorrado en 12 meses es ${montoAAno} en total tendrias ${total12}`)
