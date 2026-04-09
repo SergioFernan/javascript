e373849ee9f4cea31c856dbabdad0f1e081a9d0a
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-04-08 19:52:10 -0500

    ejercicio 8 abril array invertifo for inverso

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,13 +1,36 @@
-let number = prompt(`ingrese el numero que desea que sea multiplicado`)
-let multiplicador = prompt(`ingrese el numero por el cual desea multiplicar el numero maximo es 10`)
-if (number < 0) {
-    console.log(`el numero que desea multiplicar es menor a 0 no se puede ejecutar`)
-}
-else if (multiplicador > 10) {
-    console.log(`el numero por el cual desea multiplicar el numero es mayor a 10 no se puede ejecutar`)
-} else {
-    for (let i = 1; i <= multiplicador; i++) {
-        let multiplicacion = number * i
-        console.log(`${number} x ${i} = ${multiplicacion}`)
+﻿let palabras = [
+  "manzana",
+  "perro",
+  "casa",
+  "montana",
+  "rio enfurecido",
+  "libro",
+  "computadora",
+  "musica",
+  "sol",
+  "ciudad"
+];
+let max = []
+let invertido = []
+let min = palabras[0]
+
+for (i = 0; i < palabras.length; i++) {
+    let palabra = palabras[i]
+    console.log(`la palabra ${palabra} tiene ${palabra.length} caracteres` )
+    if (palabra.length > max.length) {
+        max = palabra;
+    }
+    else if (palabra.length < min.length) {
+        min = palabra;
     }
 }
+
+console.log(`la palabra con más caracteres es: ${max}`)
+console.log(`la palabra con menos caracteres es: ${min}`)
+
+for (i = palabras.length - 1; i >= 0; i--) {
+    let palabra = palabras[i]
+    invertido.push(palabra)
+}
+
+console.log(`el array invertido es: ${invertido}`)
