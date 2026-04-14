2460b160529674accae0c137e13770e7944bc6ed
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-04-13 19:23:30 -0500

    nueva inf en html

--- 0.2variables.js
+++ 0.2variables.js
@@ -1,42 +1,9 @@
-﻿let numeroMes = Number(prompt("Ingrese el número del mes (1-12):"));
+﻿
+let dataYear = [2024, 2026, 3000, 2100] 
 
-switch (numeroMes) {
-    case 1:
-        alert(` Es enero Verano`);
-        break;
-    case 2:
-        alert(` Es febrero Verano`);
-        break;
-    case 3:
-        alert(` Es marzo Verano`);
-        break;
-    case 4:
-        alert(` Es abril Otoño`);
-        break;
-    case 5:
-        alert(` Es mayo Otoño`);
-        break;
-    case 6:
-        alert(` Es junio Otoño`);
-        break;
-    case 7:
-        alert(` Es julio Invierno`);
-        break;
-    case 8:
-        alert(` Es agosto Invierno`);
-        break;
-    case 9:
-        alert(` Es septiembre Invierno`);
-        break;
-    case 10:
-        alert(` Es octubre Primavera`);
-        break;
-    case 11:
-        alert(` Es noviembre Primavera`);
-        break;
-    case 12:
-        alert(` Es diciembre Primavera`);
-        break;
-    default:
-        alert(` Número de mes inválido. Por favor, ingrese un número entre 1 y 12.`);
-}   
+for (i=0; i < dataYear.length; i++) {
+((dataYear[i] % 4 === 0 && dataYear[i] % 100 !== 0) || (dataYear[i] % 400 === 0))
+  ? console.log(`${dataYear[i]} es bisiesto`)
+  : console.log(`${dataYear[i]} no es un año bisiesto`);
+
+}
